f1898091d184ca5705cfd35c7ea4b6a83460a97d
wap <lukas.wappler@web.de>
2015-06-01 15:39:17 +0200

    erster Versuche mit dem Layout

--- src/main/java/fernuni/obj/ea5/a1/Rechner.java
+++ src/main/java/fernuni/obj/ea5/a1/Rechner.java
@@ -1,7 +1,10 @@
 package fernuni.obj.ea5.a1;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -23,6 +26,41 @@ public class Rechner extends Frame {
         this.setLocation(100, 100);
         this.setBackground(Color.gray);
         /* FlowLayout fuer Ziffern- und Operatorpanel setzen */
+        
+        FlowLayout flowLayout = new FlowLayout();
+        
+        Button button = new Button();
+        //button.size(50, 50);
+        
+        button.setLabel("1QWE2");
+        button.setVisible(true);
+
+        
+        Button button2 = new Button();
+        //button.size(50, 50);
+        
+        button2.setLabel("1QWE2");
+        button2.setVisible(true);
+
+        this.add(button,BorderLayout.NORTH);
+        this.add(button2, BorderLayout.WEST);
+        
+        Container container = new Container();
+        //container.setLayout(mgr);
+
+        
+        //borderLayout1.
+        //borderLayout1.addLayoutComponent("btn", button);
+        
+        //borderLayout1.addLayoutComponent(flowLayout, null);
+        
+        //flowLayout.addLayoutComponent("button", button);
+        
+        
+        
+        //this.setLayout(flowLayout);
+        
+        
         /* Zifferntasten ggf. erzeugen und in Ziffernpanel einfuegen */
         /* Operatortasten ggf. erzeugen und in Operatorpanel einfuegen */
         /* Objekte in Frame plazieren */
