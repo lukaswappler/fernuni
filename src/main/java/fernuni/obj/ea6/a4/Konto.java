b6069f344a179c713506244c2162c03fdbea5720
Lukas Wappler <lukas.wappler@web.de>
2015-06-28 19:25:59 +0200

    EA6 A4 fertig

--- src/main/java/fernuni/obj/ea6/a4/Konto.java
+++ src/main/java/fernuni/obj/ea6/a4/Konto.java
@@ -1,11 +1,19 @@
 package fernuni.obj.ea6.a4;
 
 class Konto {
-    int kontoStand = 0;
+    private int kontoStand = 0;
 
-    public void einzahlen(int betrag) {
+	synchronized public void einzahlen(int betrag) {
         int hilfsvariable = kontoStand;
         hilfsvariable = hilfsvariable + betrag;
         kontoStand = hilfsvariable;
     }
+	
+	synchronized public int getKontoStand() {
+		return kontoStand;
+	}
+
+	synchronized public void setKontoStand(int kontoStand) {
+		this.kontoStand = kontoStand;
+	}
 }
--- src/main/java/fernuni/obj/ea6/a4/NochEinThread.java
+++ src/main/java/fernuni/obj/ea6/a4/NochEinThread.java
@@ -8,6 +8,25 @@ class NochEinThread extends EinThread {
     public void run() {
         while (true) {
             /*** 1 ***/
+        	System.out.println("staatische Variable nummer: " + nummer);
+        	
+        	//Der Thread Pit und EVa bekommen beide eine Referenz desselben KontoObjectes
+        	//Auf Nummer und name des Threads von Pit ist kein Zugriff
+        	System.out.println("Pit konto: " + this.meinKonto.getKontoStand());
+        	
+        	//auf die Instanz-Variablen von TOM ist kein Zugriff möglich. Auf das Object kann von hier aus nicht zugegriffen werden.
+        	//Die Instanzvariabel sind Objektvariablen, bis auf Nummer.  
+        	        	
+        	//eigenes Konto
+        	System.out.println("Eigenes Konto also von Thread Eva: " + this.meinKonto.getKontoStand());
+        	
+        	
+        	//auf bv aus der Run Methode von EinTread ist kein Zugriff möglich. 
+        	//bv ist eine lokale Variable, die nur innerhalb von der run Methode des Threads "Ein Thread" lebt
+        	
+        	
+        	this.meinKonto.einzahlen(200);
+        	
             yield();
             try {
                 Thread.sleep(6000);
