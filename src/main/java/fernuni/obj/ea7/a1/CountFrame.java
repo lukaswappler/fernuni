70edbeaf7314605b63ac3de773df83cd605bf2f6
wap <lukas.wappler@web.de>
2015-07-08 17:27:07 +0200

    ea aufgabe 1 gelößt

--- src/main/java/fernuni/obj/ea7/a1/CountFrame.java
+++ src/main/java/fernuni/obj/ea7/a1/CountFrame.java
@@ -10,34 +10,72 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
-public class CountFrame extends Frame {
+public class CountFrame extends Frame{
 	Button bDec = new Button("<");
 	Button bInc = new Button(">");
 	Button bQuit = new Button("quit");
 	TextField tCounter = new TextField(3);
-	Counter c = new Counter(50);
+	CounterInterface c = null;
 
-	public CountFrame() {
-		tCounter.setEditable(false);
-		tCounter.setText(c.toString());
+	public CountFrame() throws RemoteException, MalformedURLException, NotBoundException {
+		
+	    final CounterInterface c = (CounterInterface) Naming
+                .lookup("//localhost/Counter");
+	    
+	    tCounter.setEditable(false);
+		tCounter.setText(c.getValue());
 		// geeignete Beobachter registrieren
 		bDec.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				c.decr();
-				tCounter.setText(c.toString());
+				try {
+                    c.decr();
+                } catch (RemoteException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+				try {
+                    tCounter.setText(c.getValue());
+                } catch (RemoteException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
 			}
 		});
 		bInc.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				c.incr();
-				tCounter.setText(c.toString());
+				try {
+                    c.incr();
+                } catch (RemoteException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+				try {
+                    tCounter.setText(c.getValue());
+                } catch (RemoteException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
 			}
 		});
 		bQuit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				c.resetCounter();
-				tCounter.setText(c.toString());
+				try {
+                    c.resetCounter();
+                } catch (RemoteException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+				try {
+                    tCounter.setText(c.getValue());
+                } catch (RemoteException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
 			}
 		});
 		addWindowListener(new WindowAdapter() {
@@ -57,7 +95,7 @@ public class CountFrame extends Frame {
 		setSize(120, 100);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
 		CountFrame countFrame = new CountFrame();
 		countFrame.setVisible(true);
 	}
--- src/main/java/fernuni/obj/ea7/a1/Counter.java
+++ src/main/java/fernuni/obj/ea7/a1/Counter.java
@@ -1,31 +1,57 @@
 package fernuni.obj.ea7.a1;
 
-class Counter {
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+class Counter extends UnicastRemoteObject implements CounterInterface {
 	private int startValue;
 	private int value;
 	private int min = 0;
 	private int max = 99;
 
-	Counter(int start) {
+	Counter(int start) throws RemoteException {
 		startValue = start;
 		value = start;
 	}
 
-	void incr() {
+	
+	public static void main(String args[]) throws Exception {
+        System.out.println("RMI server started");
+
+        try { // special exception handler for registry creation
+            LocateRegistry.createRegistry(1099);
+            System.out.println("java RMI registry created.");
+        } catch (RemoteException e) {
+            // do nothing, error means registry already exists
+            System.out.println("java RMI registry already exists.");
+        }
+
+        // Instantiate RmiServer
+
+        Counter obj = new Counter(50);
+
+        // Bind this object instance to the name "RmiServer"
+        Naming.rebind("//localhost/Counter", obj);
+        System.out.println("PeerServer bound in registry");
+    }
+	
+	public void incr() {
 		if (value < max)
 			value++;
 	}
 
-	void decr() {
+	public void decr() {
 		if (value > min)
 			value--;
 	}
 
-	void resetCounter() {
+	public void resetCounter() {
 		value = startValue;
 	}
 
-	public String toString() {
+	public String getValue() {
 		return "" + value;
 	}
 }
